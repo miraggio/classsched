5565acc795c00d49ccd0e291876e8f2560384292
Aleksej Makarov <aleksej.makarov@gmail.com>
2018-08-20 15:46:09 +0200

    xls writer: decorate as class
    Change-Id: I56b91c3544adc41afe961a6ed8cf916d286a182e

--- xls.py
+++ xls.py
@@ -1,7 +1,6 @@
 import xlsxwriter
 import local_utils as LU
 
-
 class CellColors:
     tbl = None
     size = 0
@@ -22,96 +21,127 @@ class CellColors:
             print "Error, no more colors in table"
 
 
-def get_region_columns(corner, time_start, time_step, start, end):
-    start_row = (start.minutes() - time_start) / time_step
-    stop_row = (end.minutes() - time_start) / time_step - 1
-    return [corner[0] + 1 + start_row, corner[0] + 1 + stop_row]
+class WorkBookWriter:
+    format_time = None
+    format_group = None
+    format_content = None
+    name = None
+    workbook = None
+    worksheet = None
+    first_row = 0
+    time_step_min = 10
+    colors = None
+    FIRST_COLUMN = 0
+    teacher_to_format = None
 
-#expexted "K5-6A Video 09:00 10:00 513 Mary"
-def save_sched_xls(name, items):
+    def __init__(self, name, time_step_min=10):
+        self.teacher_to_format = {}
+        self.time_step_min = time_step_min
+        self.workbook = xlsxwriter.Workbook(name)
+        self.worksheet = self.workbook.add_worksheet()
+        self.colors = CellColors()
+        self.format_time = self.workbook.add_format({'bold': True, 'align': 'center'})
+        self.format_group = self.workbook.add_format({'bold': True, 'align': 'center'})
 
-    CORNER = [0, 0]
-    TIME_STEP_MIN = 10
-    colors = CellColors()
+    def __get_region_columns(self, time_start, time_step, start, end):
+        start_row = (start.minutes() - time_start) / time_step
+        stop_row = (end.minutes() - time_start) / time_step - 1
+        return [self.first_row + 1 + start_row, self.first_row + 1 + stop_row]
 
-    workbook = xlsxwriter.Workbook(name)
-    worksheet = workbook.add_worksheet()
-    format_time = workbook.add_format({'bold': True, 'align': 'center'})
-    format_group = workbook.add_format({'bold': True, 'align': 'center'})
+    #expexted item "K5-6A * Video 09:00 10:00 513 Mary"
+    def export(cls, items):
+        book = []
+        rooms = []
+        teachers = {}
+        teacher_to_format = {}
+        groups = []
+        classes = []
 
-    book = []
-    rooms = []
-    teachers = []
-    groups = []
-    classes = []
+        start_min = 23 * 60 + 59
+        end_min = 0
 
-    start_min = 23 * 60 + 59
-    end_min = 0
+        for s in items:
+            group, ok, cl, start, stop, room, teacher = s.split()
+            start = LU.str_time(start)
+            stop = LU.str_time(stop)
 
-    for s in items:
-        print "---", s.split()
-        group, ok, cl, start, stop, room, teacher = s.split()
+            book.append([group, cl, start, stop, room, teacher])
 
-        start = LU.str_time(start)
-        stop = LU.str_time(stop)
+            if room not in rooms:
+                rooms.append(room)
+            if group not in groups:
+                groups.append(group)
+            if cl not in classes:
+                classes.append(cl)
 
-        book.append([group, cl, start, stop, room, teacher])
+            if teacher not in cls.teacher_to_format.keys():
+                color = cls.colors.get_next()
+                cls.teacher_to_format[teacher] = cls.workbook.add_format( \
+                        {'bg_color': color, 'align': 'center', 'valign': 'vcenter', 'border': 1})
 
-        if room not in rooms:
-            rooms.append(room)
-        if teacher not in teachers:
-            teachers.append(teacher)
-        if group not in groups:
-            groups.append(group)
-        if cl not in classes:
-            classes.append(cl)
+            minutes = stop.minutes() - start.minutes()
 
-        if start.minutes() < start_min:
-            start_min = start.minutes()
-        if stop.minutes() > end_min:
-            end_min = stop.minutes()
+            if teacher not in teachers.keys():
+                teachers[teacher] = minutes
+            else:
+                teachers[teacher] += minutes
 
-    groups.sort()
-    rooms.sort()
+            if start.minutes() < start_min:
+                start_min = start.minutes()
+            if stop.minutes() > end_min:
+                end_min = stop.minutes()
 
-    group_to_column = {}
-    k = 1
-    for g in groups:
-        group_to_column[g] = CORNER[0] + k
-        k += 1
+        groups.sort()
+        rooms.sort()
 
-    teacher_to_color = {}
-    for r in teachers:
-        teacher_to_color[r] = colors.get_next()
+        group_to_column = {}
+        for k in range(len(groups)):
+            group_to_column[groups[k]] = cls.FIRST_COLUMN + k + 1
 
-    row = CORNER[0] + 1
-    col = CORNER[0]
-    for t in range(start_min, end_min + 1, TIME_STEP_MIN):
-        h = t / 60
-        m = t % 60
-        worksheet.write(row, col, '{:02d}:{:02d}'.format(h, m), format_time)
-        row += 1
+        row = cls.first_row + 1
+        col = cls.FIRST_COLUMN
+        for t in range(start_min, end_min + 1, cls.time_step_min):
+            h = t / 60
+            m = t % 60
+            cls.worksheet.write(row, col, '{:02d}:{:02d}'.format(h, m), cls.format_time)
+            row += 1
 
-    row = CORNER[0]
-    col = CORNER[0] + 1
-    for g in groups:
-        worksheet.write(row, col, g, format_group)
-        col += 1
+        last_row = row
 
-    for rec in book:
-        group, cl, start, stop, room, teacher = rec
-        [r1, r2] = get_region_columns(CORNER, start_min, TIME_STEP_MIN, start, stop)
-        col = group_to_column[group]
-        color = teacher_to_color[teacher]
-        fmt = workbook.add_format({'bg_color': color, 'align': 'center', 'valign': 'vcenter', 'border': 1})
-        content = '{:s}\n{:s}\n{:s}'.format(cl, teacher, room)
-        if r1 == r2:
-            worksheet.write(r1, col, content, fmt)
+        row = cls.first_row
+        col = cls.FIRST_COLUMN + 1
+        for g in groups:
+            cls.worksheet.write(row, col, g, cls.format_group)
+            col += 1
+
+        last_col = col
+
+        for rec in book:
+            group, cl, start, stop, room, teacher = rec
+            [r1, r2] = cls.__get_region_columns(start_min, cls.time_step_min, start, stop)
+            col = group_to_column[group]
+            content = '{:s}\n{:s}\n{:s}'.format(cl, teacher, room)
+            if r1 == r2:
+                cls.worksheet.write(r1, col, content, cls.teacher_to_format[teacher])
+            else:
+                cls.worksheet.merge_range(r1, col, r2, col, content, cls.teacher_to_format[teacher])
+            if r2 > last_row:
+                last_row = r2
+
+        col = last_col + 1
+        row = cls.first_row + 1
+        for t in teachers.keys():
+            color = cls.teacher_to_format[t]
+            cls.worksheet.write(row, col, t, cls.teacher_to_format[t])
+            cls.worksheet.write(row, col + 1, teachers[t], cls.teacher_to_format[t])
+            row += 1
+
+        if row > last_row:
+            cls.first_row = row + 3
         else:
-            worksheet.merge_range(r1, col, r2, col, content, fmt)
+            cls.first_row = last_row + 3
 
-
-
-    workbook.close()
+    def save(cls):
+        cls.workbook.close()
 
 
